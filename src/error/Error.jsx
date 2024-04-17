0b764c745e8514d547f8d161a9d4b77367cecea3
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-04-17 15:07:19 +0600

    404 not found component responsive

--- src/error/Error.jsx
+++ src/error/Error.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const Error = () => {
     return (
-        <div className="text-center">
-            <h1 className="text-[350px] font-bold">404</h1>
+        <div className="text-center w-full md:w-full">
+            <h1 className=" text-[150px] md:text-[350px] font-bold">404</h1>
 
-            <h2 className="text-5xl font-bold">Not Found</h2>
+            <h2 className="text-3xl md:text-5xl font-bold">Not Found</h2>
 
             <button className="bg-secondary p-4 rounded-xl mt-6"><Link to="/">Back to Home</Link></button>
         </div>
