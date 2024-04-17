abb76da09db3896178d142b59a98ea1113d33961
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-04-17 12:19:51 +0600

    add heading for extra section

--- src/pages/SectionSecond.jsx
+++ src/pages/SectionSecond.jsx
@@ -6,7 +6,7 @@ const SectionSecond = () => {
         <div className="mt-12">
             <div className="bg-gradient-to-b from-[#2e0249] to-[#680871] py-16 px-6 font-[sans-serif]">
       <div className="max-w-6xl mx-auto text-center text-white">
-        <h2 className="md:text-5xl text-4xl font-extrabold mb-6">Join Our Exclusive Newsletter</h2>
+        <h2 className="md:text-5xl text-4xl font-extrabold mb-6">Booking Our Exclusive Hotel</h2>
         <p className="text-xl text-gray-300">Be part of our elite community. Get VIP access to curated content, early product releases, and special promotions.</p>
         <div className="bg-white shadow-lg rounded-lg p-8 mt-12 flex flex-col md:flex-row items-center justify-center">
           <input type="email" placeholder="Enter your email" className="w-full md:w-96 bg-transparent border-b-2 border-[#a91079] py-3 px-4 text-[#2e0249] text-base focus:outline-none placeholder-[#a91079] placeholder-opacity-70" />
